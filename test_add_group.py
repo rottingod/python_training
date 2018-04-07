a8845f99028c820fae1df9a58637b0f750693591
Елена Плотникова <rottingod@gmail.com>
2018-04-07 19:11:39 +0300

    Записан рекордером сценарий

---
+++ test_add_group.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_test_add_group(self):
+        success = True
+        wd = self.wd
+        wd.get("https://addons.mozilla.org/en-GB/firefox/addon/selenium-builder/")
+        self.assertTrue(success)
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
