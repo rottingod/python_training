af7c65b5f740a2772ff2b637ceaf55f11c91ba63
Елена Плотникова <rottingod@gmail.com>
2018-04-09 22:15:16 +0300

    Создан тест для создания контакта

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+
+    def __init__(self, name, middle_name, last_name, nickname, company, address, home, mobile, email):
+        self.name = name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.email = email
