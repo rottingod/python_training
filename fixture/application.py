4744e98a2a971de0f17c9a9ec47f2baf8d0738b1
Елена Плотникова <rottingod@gmail.com>
2018-04-19 22:19:05 +0300

    Выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,6 +7,7 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
             wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from fixture.application import Application
 import pytest
-from contact import Contact
+from model.contact import Contact
 
 
 @pytest.fixture
@@ -11,11 +11,11 @@ def app(request):
     return fixture
 
 
-def test_test_add_contact(app):
+def test_add_contact(app):
     contact = Contact(name="Elena", middle_name="Igorevna", last_name="Plotnikova", nickname="rottingod",
                       company="python_training", address="Moscow",
                       home="89991234567", mobile="89991234567", email="rottingod@gmail.com")
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(contact)
-    app.logout()
+    app.session.logout()
 
