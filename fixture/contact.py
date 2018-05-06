dee5b7ffbc9f7a6e200b94e02ca3dd6e98025145
Елена Плотникова <rottingod@gmail.com>
2018-05-06 18:25:05 +0300

    удаление контакта проходит

--- fixture/contact.py
+++ fixture/contact.py
@@ -60,8 +60,7 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
-            wd.find_element_by_link_text("home").click()
+        wd.find_element_by_link_text("home").click()
 
     def get_contact_list(self):
         wd = self.app.wd
