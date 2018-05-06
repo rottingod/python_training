00c72e44048d6e25bb334d88b51d39ae0506b77b
Елена Плотникова <rottingod@gmail.com>
2018-05-06 19:45:56 +0300

    Реализованы проверки путем сравнения списков для групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,6 +4,14 @@ from model.contact import Contact
 def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    app.contact.modify_first_contact(Contact(name="Maria"))
 
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(name="Maria")
+    contact.id = old_contacts[0].id
+    contact.last_name = old_contacts[0].last_name
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
