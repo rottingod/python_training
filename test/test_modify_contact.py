1a061d7590591447a9473a4f2be52b19c53c92e2
Елена Плотникова <rottingod@gmail.com>
2018-05-07 23:24:55 +0300

    Реализовано кеширование при загрузке списков групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -10,8 +10,8 @@ def test_modify_first_contact(app):
     contact.id = old_contacts[0].id
     contact.last_name = old_contacts[0].last_name
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
