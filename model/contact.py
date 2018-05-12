3f7f9e6ca00fed6b6a6e70cae899809288e05b48
Елена Плотникова <rottingod@gmail.com>
2018-05-12 16:36:58 +0300

    Параметризованы тесты для создания групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -24,7 +24,9 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % (
+        self.id, self.firstname, self.lastname, self.middlename, self.company, self.address, self.homephone,
+        self.workphone, self.mobilephone, self.secondaryphone, self.email, self.email2, self.email3)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return (prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])).rstrip()
+
+
+testdata = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+                   lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                   company=random_string("company", 10), address=random_string("address", 10),
+                   homephone=random_string("homephone", 10), mobilephone=random_string("mobilephone", 10),
+                   workphone=random_string("workphone", 10), secondaryphone=random_string("secondaryphone", 10),
+                   email=random_string("email", 10), email2=random_string("email2", 10),
+                   email3=random_string("email3", 10))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Elena", middlename="Igorevna", lastname="Plotnikova", nickname="rottingod",
-                      company="python_training", address="Moscow",
-                      homephone="89991234567", mobilephone="89991234567", workphone="12345",
-                      secondaryphone="12345678", email="rottingod@gmail.com", email2="12345@gmail.com",
-                      email3="123@gmail.com")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
+    print(contact)
+    print(new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
