bbb9ae690432e639986e3cbff0cbf9a0a27b5ddb
Елена Плотникова <rottingod@gmail.com>
2018-05-12 19:58:07 +0300

    Падает тест

--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,8 @@ class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, company=None, address=None,
                  homephone=None, mobilephone=None, workphone=None, secondaryphone=None, email=None, email2=None,
-                 email3=None, id=None, all_phones_from_home_page=None, all_emails_from_home_page=None):
+                 email3=None, id=None, all_phones_from_home_page=None, all_emails_from_home_page=None,
+                 all_phones_from_view_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -22,11 +23,12 @@ class Contact:
         self.id = id
         self.all_phones_from_home_page = all_phones_from_home_page
         self.all_emails_from_home_page = all_emails_from_home_page
+        self.all_phones_from_view_page = all_phones_from_view_page
 
     def __repr__(self):
         return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % (
-        self.id, self.firstname, self.lastname, self.middlename, self.company, self.address, self.homephone,
-        self.workphone, self.mobilephone, self.secondaryphone, self.email, self.email2, self.email3)
+            self.id, self.firstname, self.lastname, self.middlename, self.company, self.address, self.homephone,
+            self.workphone, self.mobilephone, self.secondaryphone, self.email, self.email2, self.email3)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
