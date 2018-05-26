9dbe08b1fc8cdcabf3072975d19347992a8ff42e
Елена Плотникова <rottingod@gmail.com>
2018-05-26 23:56:38 +0300

    Проблема с add_group

---
+++ fixture/db.py
@@ -0,0 +1,36 @@
+from model.group import Group
+import pymysql.cursors
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host,
+                                          database=name,
+                                          user=user,
+                                          password=password,
+                                          charset='utf8mb4',
+                                          cursorclass=pymysql.cursors.DictCursor)
+        self.connection.autocommit = True
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                id = row['group_id']
+                name = row['group_name']
+                header = row['group_header']
+                footer = row['group_footer']
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
